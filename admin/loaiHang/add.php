296d9b4a7fdf2c3ac90aced960451fd3d4b210e2
Nguyen Tuan Anh <tuananh31j@gmail.com>
2023-05-27 15:27:58 +0700

    them moi loai hang

---
+++ admin/loaiHang/add.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>thêm loại:</h1>
+    <form action="index.php?act=addlh" method="post">
+        <input type="text" name="maLoai" disabled placeholder="mã loại"><br>
+        <input type="text" name="tenLoai" placeholder="tên loại"><br>
+        <input type="submit" name="btn-them" value="Thêm">
+    </form>
+</div>
